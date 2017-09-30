ef50d09d9c3a9a95f3fa884b8c7afeb454fafd6d
GRicha2380 <gregor.richardson@gmail.com>
2017-09-30 15:29:53 -0700

    basic framework for goals site

---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict'; // no sloppy JS habits allowed here.
+/*
+Things to do:
+	* 'Why' button should create div element with class whyBox. CSS will style it properly as a modal.
+	* 'Why' button will be event listener that does local ajax call to JSON file
+	* 'Why' modal will have link to github repo, explination about 201 course, thank you to Rachel
+	* Global goals will call results from Google sheets JSON file
+	* 'My Goals' will use local storage
+	* Field validation for 'Send' button via event handler
+*/
