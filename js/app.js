71f5cad74dd364fc0dd9421679d089b96f855441
GRicha2380 <gregor.richardson@gmail.com>
2017-10-01 12:58:17 -0700

    wow, localstorage actually works

--- js/app.js
+++ js/app.js
@@ -10,6 +10,19 @@ Things to do:
 */
 document.addEventListener('DOMContentLoaded', function(event) {
   var showWhyBox = false;
+  var goalArray; // initialize local array for holding goals
+  // if existing values are in localhost, use them
+  if (window.localStorage.getItem('goals')) {
+    console.log('Found existing localstorage values.',JSON.parse(window.localStorage.getItem('goals')));
+    goalArray = JSON.parse(window.localStorage.getItem('goals'));
+    populateGoals(); // fill page with goal values
+  }
+  // otherwise make a blank array
+  else {
+    console.log('No local storage, starting fresh.');
+    goalArray = [];
+  }
+
   // var newDiv = document.createElement('div');
   // var body = document.querySelector('body');
   var whyBox = document.querySelector('#whyBox');
@@ -17,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
   console.log('DOM fully loaded and parsed');
 
 
-  function makeNewDiv(jsonData, idName) {
+  function makeNewDiv(jsonData, idName, modal) {
     // parse json first
     function processJson(jsonData) {
       console.log('reading JSON, sir');
@@ -39,16 +52,21 @@ document.addEventListener('DOMContentLoaded', function(event) {
     newDiv.innerHTML = processJson(jsonData);
     // document.querySelector('body').appendChild(newDiv);
     body.insertBefore( newDiv, body.firstChild );
+    // if modal paramater was passed, set document class
+    if (modal) {
+      body.classList.toggle('modal');
+    }
   }
 
   document.querySelector('nav a').addEventListener('click', function(){
 
-
+    // if box variable is set to false
     if (!showWhyBox) {
       console.log('clicked');
       var xmlhttp = new XMLHttpRequest();
       var url = 'data/whyBox.json';
 
+      // AJAX request to load modal content
       xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var whyBoxData = JSON.parse(this.responseText);
@@ -57,14 +75,16 @@ document.addEventListener('DOMContentLoaded', function(event) {
           // newDiv.setAttribute('id', 'whyBox');
           // newDiv.innerHTML = processJson(whyBoxData);
           // document.querySelector('body').appendChild(newDiv);
-          makeNewDiv(whyBoxData,'whyBox');
+          makeNewDiv(whyBoxData,'whyBox','modal');
           showWhyBox = true;
           document.querySelector('.close').addEventListener('click', function(event) {
+            // note: I can turn this into a function
             showWhyBox = false;
             document.querySelector('body').removeChild(document.querySelector('#whyBox'));
+            document.querySelector('body').classList.toggle('modal');
           });
           document.querySelector('#whyBox').addEventListener('click', function(e) {
-          // click on background  
+          // click on background?
           });
         } else {
           console.log('no data');
@@ -76,5 +96,35 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
   }); // end onclick
 
+  // When the send button is pressed
+  document.querySelector('.send').addEventListener('click', function(event){
+    // grab values from input fields, save to array
+    goalArray.push({
+      'goal':document.querySelector('#yourGoal').value,
+      'author':document.querySelector('#userName').value});
+
+    // send array to localStorage function
+    saveLocalStorage(goalArray);
+    document.querySelector('#yourGoal').value = '';
+    document.querySelector('#userName').value = '';
+    alert('thanks for subitting');
+  }); // end send button event listener
+
+  // save the string
+  function saveLocalStorage(itemName) {
+    window.localStorage.setItem('goals', JSON.stringify(itemName));
+    console.log('prove it exists by typing:','JSON.parse(window.localStorage.getItem(\'goals\'))');
+  }
+
+  // read the string
+  function readLocalStorage() {
+    //var storedNames = JSON.parse(localStorage.getItem("names"));
+    //return window.localStorage.getItem('status');
+    return JSON.parse(window.localStorage.getItem('goals'));
+  }
+
+  function populateGoals() {
+
+  }
 
 }); // end document ready
